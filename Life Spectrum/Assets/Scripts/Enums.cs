9cfe0743daa2a4f4694bb0ec7d7c24b5d026fcee
mmeo009 <shh020615@gmail.com>
2024-04-11 19:43:41 +0900

    [feat] scriptable 제작

---
+++ Life Spectrum/Assets/Scripts/Enums.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Define
+{
+    public class Enums
+    {
+        public enum PlayerStats
+        {
+            life,
+            fullness,
+            fell,
+            money
+        }
+    }
+}
+
---
+++ Life Spectrum/Assets/Scripts/StoryObject.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Define;
+
+[CreateAssetMenu(fileName = "NewStory", menuName = "ScriptableObject/StoryObject")]
+public class StoryObject : ScriptableObject
+{
+    public int storyNum;
+
+    public string storyName;
+
+    [TextArea (10,10)]
+    public string story;
+    public List<AdditionalStats> additionalStats = new List<AdditionalStats>();
+
+    [System.Serializable]
+    public class AdditionalStats
+    {
+        public Enums.PlayerStats additionalStat;
+        public float amount;
+    }
+}
