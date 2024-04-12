42958b0ac068152c7286c1ec7e315280ffc1db3f
mmeo009 <shh020615@gmail.com>
2024-04-12 23:45:27 +0900

    [feat] 평오는 바부야

--- Life Spectrum/Assets/Scripts/Stats.cs
+++ Life Spectrum/Assets/Scripts/Stats.cs
@@ -6,6 +6,13 @@ namespace LifeSpectrum
 {
     public class Stats
     {
+        //======================기본 스탯=====================
+        public int statLife;                    // 생명력 스탯
+        public int statFullness;                // 포만감 스탯
+        public int statFeel;                    // 행복도 스탯
+
+        //======================부가 스탯=====================
+        public int statSociability;
 
     }
 }
