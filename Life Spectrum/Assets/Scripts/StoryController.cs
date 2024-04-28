d1a9a7db2adb6fbe8f9469fdcd8228f45154bf41
mmeo009 <shh020615@gmail.com>
2024-04-28 23:16:42 +0900

    [Feat] 왼쪽 회전이 왜 안될까.... Dotween설치

--- Life Spectrum/Assets/Scripts/StoryController.cs
+++ Life Spectrum/Assets/Scripts/StoryController.cs
@@ -8,28 +8,20 @@ namespace LifeSpectrum
 {
     public class StoryController : MonoBehaviour
     {
+
+
         public StoryObject story;
         public GameManager gameManager;
-
-        public TMP_Text statText;
-        public TMP_Text storyNameText;
-        public TMP_Text storyText;
-        public TMP_Text leftButtonText;
-        public TMP_Text rightButtonText;
-
         public GameObject card;
         public bool isLeft = false;
-
-        [SerializeField] private Vector3 curveStart = new Vector3(-4f, -2, 0);
-        [SerializeField] private Vector3 curveEnd = new Vector3(-4f, -2, 0);
-        [SerializeField] private List<Vector3> curveDots = new List<Vector3>();
-
+        private Vector3 lastMousePosition;
+        private Vector3 curveNormal;
 
         private void Start()
         {
             gameManager = GameManager.Instance;
-            curveDots = GenerateCurvePoints(curveStart, new Vector3(0, 1f, 0), curveEnd, 25);
         }
+    
 
         void Update()
         {
@@ -75,97 +67,54 @@ namespace LifeSpectrum
 
         private void StartMouseDrag()
         {
-            List<float> angles = new List<float>();
-
-            float minAngle = -20f;
-            float maxAngle = 20f;
-
-
             var target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
             var delta = 40 * Time.deltaTime;
+            float rotationAngle = 0;
+            float maxRotation;
+            float actualRotation;
+            float rotationDirection = 0;
 
             if (target.x < 0)
             {
                 isLeft = true;
-                leftButtonText.color = Color.red;
-                rightButtonText.color = new Color(0.2735849f, 0.2735849f, 0.2735849f, 1);
 
                 if (target.x < -1)
                 {
                     target.x = -1;
                 }
+                rotationAngle = Mathf.Lerp(0, -13, Mathf.InverseLerp(0, -1, target.x));
+                rotationDirection = -1;
             }
             else if (target.x > 0)
             {
                 isLeft = false;
-                rightButtonText.color = Color.red;
-                leftButtonText.color = new Color(0.2735849f, 0.2735849f, 0.2735849f, 1);
 
                 if (target.x > 1)
                 {
                     target.x = 1;
                 }
+                rotationAngle = Mathf.Lerp(0, 13, Mathf.InverseLerp(0, 1, target.x));
+                rotationDirection = 1;
             }
 
+            maxRotation = Mathf.Abs(rotationAngle - card.transform.rotation.eulerAngles.x);
+            actualRotation = Mathf.Clamp(delta * maxRotation, 0, maxRotation);
 
-            var modifiedVactor = new Vector3(target.x, -1, -0.5f);
+            var modifiedVector = new Vector3(target.x, -1, -0.5f);
 
-            delta *= Vector3.Distance(transform.position, modifiedVactor);
-            card.transform.position = Vector3.MoveTowards(card.transform.position, modifiedVactor, delta);
+            delta *= Vector3.Distance(transform.position, modifiedVector);
+            card.transform.position = Vector3.MoveTowards(card.transform.position, modifiedVector, delta);
+            card.transform.rotation = Quaternion.Euler(card.transform.rotation.eulerAngles.x + rotationDirection * actualRotation, 90, -90);
         }
         private void EndMouseDrag()
         {
             card.transform.position = new Vector3(0, -1, -0.5f);
+            card.transform.rotation = Quaternion.Euler(0, 90, -90);
             Debug.Log((isLeft == true) ? "¿ÞÂÊ" : "¿À¸¥ÂÊ");
             isLeft = false;
             card = null;
-            rightButtonText.color = new Color(0.2735849f, 0.2735849f, 0.2735849f, 1);
-            leftButtonText.color = new Color(0.2735849f, 0.2735849f, 0.2735849f, 1);
         }
 
-        public List<Vector3> GenerateCurvePoints(Vector3 startPoint, Vector3 controlPoint, Vector3 endPoint, int numPoints)
-        {
-            List<Vector3> curvePoints = new List<Vector3>();
-
-            if (numPoints <= 5) numPoints = 5;
-
-
-            for (int i = 0; i <= numPoints; i++)
-            {
-                float t = i / (float)numPoints;
-                Vector3 point = GetCurvePoint(startPoint, controlPoint, endPoint, t);
-                curvePoints.Add(point);
-
-            }
-
-            return curvePoints;
-        }
-
-        public Vector3 GetCurvePoint(Vector3 a, Vector3 b, Vector3 c, float t)
-        {
-            t = Mathf.Clamp01(t);
-            float oneMinusT = 1f - t;
-            return (oneMinusT * oneMinusT * a) + (4f * oneMinusT * t * b) + (t * t * c);
-        }
-
-        private void OnDrawGizmos()
-        {
-            Gizmos.matrix = transform.localToWorldMatrix;
-            Gizmos.color = Color.blue;
-
-            Gizmos.DrawSphere(curveStart, 0.03f);
-            //Gizmos.DrawSphere(Vector3.zero, 0.03f);
-            Gizmos.DrawSphere(curveEnd, 0.03f);
-
-            Vector3 p1 = curveStart;
-            for (int i = 0; i < 20; i++)
-            {
-                float t = (i + 1) / 20f;
-                Vector3 p2 = GetCurvePoint(curveStart, new Vector3(0, 1f, 0), curveEnd, t);
-                Gizmos.DrawLine(p1, p2);
-                p1 = p2;
-            }
-        }
     }
 }
