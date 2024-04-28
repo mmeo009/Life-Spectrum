7be53f25df6cf0c80eece20d313e75d3637aceda
mmeo009 <shh020615@gmail.com>
2024-04-29 00:22:51 +0900

    [feat] 스크립터블오브젝트 다시 만들었어요

--- Life Spectrum/Assets/Scripts/Enums.cs
+++ Life Spectrum/Assets/Scripts/Enums.cs
@@ -11,7 +11,8 @@ namespace LifeSpectrum
             Intelligence,
             Strength,
             Personality,
-            Money
+            Money,
+            Age
         }
         public enum ActonMethod
         {
@@ -20,6 +21,35 @@ namespace LifeSpectrum
             Multiply,
             Divide,
             Set
+        }
+        /// <summary>
+        /// Type_A : 자연 생성 스토리
+        /// Type_B : 나이, 연령대에 따라서 등장하는 스토리
+        /// Type_C : 스토리 선택지에 따라 새롭게 발견되는 스토리
+        /// Type_D : 특정 스탯의 수치에 따라 등장하는 스토리.
+        /// </summary>
+        public enum StoryType
+        {
+            Type_A,
+            Type_B,
+            Type_C,
+            Type_D
+        }
+
+        /// <summary>
+        /// All : 모든 나이
+        /// Infancy : 유아기
+        /// Adolescence : 애새기
+        /// Adulthood : 어른
+        /// Elderly : 용진이 동년배
+        /// </summary>
+        public enum Age
+        {
+            All,
+            Infancy, 
+            Adolescence, 
+            Adulthood, 
+            Elderly
         }
         public enum StoryStangeDegree
         {
--- Life Spectrum/Assets/Scripts/StoryObject.cs
+++ Life Spectrum/Assets/Scripts/StoryObject.cs
@@ -7,21 +7,33 @@ namespace LifeSpectrum
     [CreateAssetMenu(fileName = "StoryCard", menuName = "ScriptableObject/StoryObject")]
     public class StoryObject : ScriptableObject
     {
-        public List<Option> options = new List<Option>();
-    }
-    [System.Serializable]
-    public class Story
-    {
-
+        [Header("이야기 텍스트")] [TextArea(10, 10)] public string StoryText;
+        [Header("스토리 등장 조건 스텟")] public List<StatMin> statMins = new List<StatMin>();
+        [Header("이전 스토리가 존재하는지")] public bool hasPreviousStory = false;
+        [Header("이후 스토리")] public StoryObject nextStory;
+        [Header("선택지들(ALL선택지 최소 2개)")] public List<Option> options = new List<Option>();
     }
     [System.Serializable]
     public class Option
     {
         [Header("옵션 텍스트")][TextArea(10, 10)] public string OptionText;
         [Header("좋은(긍정적인) 질문인지")] public bool isPositive;
-        [Header("바꿀 스텟")] public Enums.PlayerStats Stat;
+        [Header("선택 이후 스토리")] public StoryObject nextStory;
+        [Header("등장하는 나이")] public Enums.Age age;
+        [Header("옵션에서 바꿀 스텟")] public List<Stat> stats = new List<Stat>();
+    }
+    [System.Serializable]
+    public class Stat
+    {
+        [Header("바꿀 스텟")] public Enums.PlayerStats StatType;
         [Header("바꿀 양")] public int Amount;
         [Header("바꿀 방법")] public Enums.ActonMethod Method;
+    }
+    [System.Serializable]
+    public class StatMin
+    {
+        [Header("조건 스텟")] public Enums.PlayerStats StatType;
+        [Header("최소 양")] public int Amount;
     }
 }
 
