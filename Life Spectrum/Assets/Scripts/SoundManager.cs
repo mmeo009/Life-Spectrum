83cae879d28c846507a8b07a25ad4f39f90e85e8
mmeo009 <shh020615@gmail.com>
2024-06-04 18:05:02 +0900

    [Feat] SoundManager 노래 불러오기

--- Life Spectrum/Assets/Scripts/SoundManager.cs
+++ Life Spectrum/Assets/Scripts/SoundManager.cs
@@ -1,18 +1,127 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+using UnityEditor.SceneManagement;
+using Unity.VisualScripting;
 
 public class SoundManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static SoundManager Instance;
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private List<SoundGroup> backGroundMusics = new List<SoundGroup>();
+    [SerializeField] private List<SoundGroup> inGameSounds = new List<SoundGroup>();
+    [SerializeField] private AudioSource AudioSource;
+
+    private void Awake()
     {
-        
+        if (Instance)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        else
+        {
+            transform.parent = null;
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        AudioSource = this.AddComponent<AudioSource>();
+        LoadSoundData();
+    }
+    public void LoadSoundData()
+    {
+        var bgSounds = Resources.LoadAll<AudioClip>("Sound/BackgroundMusic");
+        var sfx = Resources.LoadAll<AudioClip>("Sound/SFX");
+
+        List<AudioClip> Infancy = new List<AudioClip>();
+        List<AudioClip> Adolescenece = new List<AudioClip>();
+        List<AudioClip> Youth = new List<AudioClip>();
+        List<AudioClip> MiddleAge = new List<AudioClip>();
+        List<AudioClip> Elderly = new List<AudioClip>();
+
+        foreach (var sound in bgSounds)
+        {
+            if (sound.name.Contains("Infancy"))
+            {
+                Infancy.Add(sound);
+            }
+            else if(sound.name.Contains("Adolescenece"))
+            {
+                Adolescenece.Add(sound);
+            }
+            else if(sound.name.Contains("Youth"))
+            {
+                Youth.Add(sound);
+            }
+            else if(sound.name.Contains("MiddleAge"))
+            {
+                MiddleAge.Add(sound);
+            }
+            else if (sound.name.Contains("Elderly"))
+            {
+                Elderly.Add(sound);
+            }
+            else
+            {
+                Debug.LogWarning("이름을 똑바로 적어주세요 ^^");
+            }
+        }
+
+        backGroundMusics.Add(new SoundGroup("Infancy", Infancy));
+        backGroundMusics.Add(new SoundGroup("Adolescenece", Adolescenece));
+        backGroundMusics.Add(new SoundGroup("Youth", Youth));
+        backGroundMusics.Add(new SoundGroup("MiddleAge", MiddleAge));
+        backGroundMusics.Add(new SoundGroup("Elderly", Elderly));
+
+        List<AudioClip> GameOver = new List<AudioClip>();
+        List<AudioClip> Paper = new List<AudioClip>();
+        List<AudioClip> PopUp = new List<AudioClip>();
+        foreach(var sound in sfx)
+        {
+            if(sound.name.Contains("GameEnd"))
+            {
+                GameOver.Add(sound);
+            }
+            else if(sound.name.Contains("Paper"))
+            {
+                Paper.Add(sound);
+            }
+            else if(sound.name.Contains("PopUp"))
+            {
+                PopUp.Add(sound);
+            }
+            else
+            {
+                Debug.LogWarning("이름을 잘 만드세요 ^^");
+            }
+        }
+
+        inGameSounds.Add(new SoundGroup("GameEnd", GameOver));
+        inGameSounds.Add(new SoundGroup("Paper", Paper));
+        inGameSounds.Add(new SoundGroup("PopUp", PopUp));
+    }
+    public void PlaySound(string name, bool isBGM) 
+    {
+        if(isBGM == true)
+        {
+            var sg = backGroundMusics.Find(sg =>sg.name == name);
+            if(sg.audioClips.Count > 1)
+            {
+
+            }
+        }
+    }
+}
+[System.Serializable]
+public class SoundGroup
+{
+    public string name;
+    public List<AudioClip> audioClips;
+    public SoundGroup(string name, List<AudioClip> list)
+    {
+        this.name = name;
+        this.audioClips = list;
     }
 }
